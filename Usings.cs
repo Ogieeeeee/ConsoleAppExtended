67845dcddf545df413b3bd217b20ece49ec22259
Ogie <o.kelleci@live.nl>
2022-05-21 23:39:56 +0200

    Added:
    Logging
    Dependency Injection
    Several libraries
    appsettings.json
    Global using namespaces

---
+++ Usings.cs
@@ -0,0 +1,8 @@
+﻿global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.Logging;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+
+global using Serilog;
+
+global using ConsoleAppExtended;
